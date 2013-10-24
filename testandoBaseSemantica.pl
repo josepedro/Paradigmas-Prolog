a4542548d3e761b0f45ed7c39642cd60041054fc
Yeltsin Suares <yeltsin.lobato@hotmail.com>
2013-10-23 23:03:01 -0200

    testanto base semanatica

---
+++ testandoBaseSemantica.pl
@@ -0,0 +1,43 @@
+:-op(900,xfx,[ehUmTipoDe,ehUm,tem,temUma]).
+
+acorde temUma terca.
+
+acordeMaior ehUmTipoDe acorde.
+acordeMenor ehUmTipoDe acorde.
+
+
+doMaior ehUm acordeMaior.
+reMaior ehUm acordeMaior.
+miMaior ehUm acordeMaior.
+faMaior ehUm acordeMaior.
+solMaior ehUm acordeMaior.
+laMaior ehUm acordeMaior.
+siMaior ehUm acordeMaior.
+
+doMenor ehUm acordeMenor.
+reMenor ehUm acordeMenor.
+miMenor ehUm acordeMenor.
+faMenor ehUm acordeMenor.
+solMenor ehUm acordeMenor.
+laMenor ehUm acordeMenor.
+siMenor ehUm acordeMenor.
+
+doMaior tem tercaMaior=mi.
+reMaior tem tercaMaior=solb.
+miMaior tem tercaMaior=lab.
+faMaior tem tercaMaior=la.
+solMaior tem tercaMaior=si.
+laMaior tem tercaMaior=reb.
+siMaior tem tercaMaior=mib.
+
+doMenor tem tercaMenor=mib.
+reMenor tem tercaMenor=fa.
+miMenor tem tercaMenor=sol.
+faMenor tem tercaMenor=lab.
+solMenor tem tercaMenor=sib.
+laMenor tem tercaMenor=do.
+siMenor tem tercaMenor=re.
+
+X ehUm Z2:-Z1 ehUmTipoDe Z2, X ehUm Z1. 
+X tem Atributo=Valor:-X ehUm C, C tem Atributo=Valor. 
+
