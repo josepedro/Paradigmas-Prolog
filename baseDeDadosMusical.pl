67142997f0b73d9e71d91633b75bbd572725df90
Gepeto <1jpsneto@gmail.com>
2013-10-25 00:22:20 -0200

    Base de dados extendidas para acordes Maiores, Menores, Aumentados e Diminutos para todos os tons possiveis.

--- baseDeDadosMusical.pl
+++ baseDeDadosMusical.pl
@@ -25,57 +25,93 @@ quinta_justa(sol,re).
 quinta_justa(la,mi).
 quinta_justa(si,solb).
 
+% Definicao de quintas aumentadas
+quinta_aumentada(do,lab).
+quinta_aumentada(re,sib).
+quinta_aumentada(mi,do).
+quinta_aumentada(fa,reb).
+quinta_aumentada(sol,mib).
+quinta_aumentada(la,fa).
+quinta_aumentada(si,solb).
+
+% Definicao de quintas diminutas
+quinta_diminuta(do,solb).
+quinta_diminuta(re,lab).
+quinta_diminuta(mi,sib).
+quinta_diminuta(fa,si).
+quinta_diminuta(sol,reb).
+quinta_diminuta(la,mib).
+quinta_diminuta(si,fa).
+
+% Conhecimento para saber as notas de um determinado acorde
+cM(do,mi,sol).
+cm(do,mib,sol).
+caum(do,mi,lab).
+cdim(do,mib,solb).
+
+dbM(reb,fa,lab).
+dbm(reb,mi,lab).
+dbaum(reb,fa,la).
+dbdim(reb,mi,la).
+
+dM(re,solb,la).
+dm(re,fa,la).
+daum(re,solb,sib).
+ddim(re,fa,lab).
+
+ebM(mib,sol,sib).
+ebm(mib,solb,sib).
+ebaum(mib,sol,si).
+ebdim(mib,solb,la).
+
+eM(mi,lab,si).
+em(mi,sol,si).
+eaum(mi,lab,do).
+edim(mi,sol,sib).
+
+fM(fa,la,do).
+fm(fa,lab,do).
+faum(fa,la,reb).
+fdim(fa,lab,si).
+
+gbM(solb,sib,reb).
+gbm(solb,la,reb).
+gbaum(solb,sib,re).
+gbdim(solb,la,do).
+
+gM(sol,si,re).
+gm(sol,sib,re).
+gaum(sol,si,mib).
+gdim(sol,sib,reb).
+
+abbM(lab,do,mib).
+abm(lab,si,mib).
+abaum(lab,do,mi).
+abdim(lab,si,re).
+
+aM(la,reb,mi).
+am(la,do,mi).
+aaum(la,reb,fa).
+adim(la,do,mib).
+
+bbM(sib,re,fa).
+bbm(sib,reb,fa).
+bbaum(sib,re,solb).
+bbdim(sib,reb,mi).
+
+bM(si,mib,solb).
+bm(si,re,solb).
+baum(si,mib,sol).
+bdim(si,re,fa).
+
+
 % Definicao de acordes maiores
 acorde_maior(TONICA,TERCA,QUINTA):-
 		terca_maior(TONICA,TERCA),
 		quinta_justa(TONICA,QUINTA).
-% acorde_maior(desconhecida,NOTA1,NOTA2).
-% acorde_maior(TONICA,desconhecida,NOTA2).
-% acorde_maior(TONICA,NOTA1,desconhecida).
 
 % Definicao de acordes menores
 acorde_menor(TONICA,TERCA,QUINTA):-
 		terca_menor(TONICA,TERCA),
 		quinta_justa(TONICA,QUINTA).
-% acorde_menor(desconhecida,TERCA,QUINTA).
-% acorde_menor(TONICA,desconhecida,QUINTA).
-% acorde_menor(TONICA,TERCA,desconhecida).
 
-% Definicao dos acordes em relacao as suas tercas maiores
-acorde(do_Maior,terca_maior(do,mi)).
-acorde(re_Maior,terca_maior(re,solb)).
-acorde(mi_Maior,terca_maior(mi,lab)).
-acorde(fa_Maior,terca_maior(fa,la)).
-acorde(sol_Maior,terca_maior(sol,si)).
-acorde(la_Maior,terca_maior(la,reb)).
-acorde(si_Maior,terca_maior(si,mib)).
-
-% Definicao dos acordes em relacao as suas tercas maiores
-acorde(do_Menor,terca_menor(do,mib)).
-acorde(re_Menor,terca_menor(re,fa)).
-acorde(mi_Menor,terca_menor(mi,sol)).
-acorde(fa_Menor,terca_menor(fa,lab)).
-acorde(sol_Menor,terca_menor(sol,sib)).
-acorde(la_Menor,terca_menor(la,do)).
-acorde(si_Menor,terca_menor(si,re)).
-
-% Relacao dos acordes maiores com tonica, terca e quinta
-acordeMaior(ACORDE,TONICA,TERCA,QUINTA):-
-		acorde(ACORDE,terca_maior(TONICA,TERCA)),
-		quinta(TONICA,QUINTA).
-
-% Relacao dos acordes menores com tonica, terca e quinta
-acordeMenor(ACORDE,TONICA,TERCA,QUINTA):-
-		acorde(ACORDE,terca_menor(TONICA,TERCA)),
-		quinta(TONICA,QUINTA).
-
-
-		
-
-
-
-
-
-
-
-		
